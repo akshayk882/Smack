2ccce3dc3676c89925e20adbfb5bfbd3438c9660
akshay <akshayk882.com>
2019-01-03 19:24:54 +0530

    Main Activity

--- app/src/main/java/com/akshaykumbhar/smack/MainActivity.kt
+++ app/src/main/java/com/akshaykumbhar/smack/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.akshaykumbhar.smack
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -20,11 +17,10 @@ class MainActivity : AppCompatActivity() {
 
 
         val toggle = ActionBarDrawerToggle(
-                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+                this, drawer_layout, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
-        nav_view.setNavigationItemSelectedListener(this)
     }
 
     override fun onBackPressed() {
@@ -34,22 +30,17 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+    fun LoginbtnNavClk(view: View)
+    {
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
+    fun addChannelBtn(view: View)
+    {
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
     }
+    fun SendmsgBtnclk(view: View)
+    {
 
+    }
 
 }
